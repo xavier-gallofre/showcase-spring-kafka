108ddca05b7098dc1c90bd85fa3aeb19b6d6c9a7
Xavier Gallofré <xavier.gallofre@gmail.com>
2024-09-04 19:05:50 +0200

    Initial commit

---
+++ src/main/java/es/xganie/showcase/ShowcaseSpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package es.xganie.showcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowcaseSpringKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowcaseSpringKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/xganie/showcase/ShowcaseSpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.xganie.showcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowcaseSpringKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
