8e94f1e49fcadb5e360c4d99af265a837871686a
Xavier Gallofré <xavier.gallofre@gmail.com>
2024-09-04 19:52:39 +0200

    Add awaitility to wait for a mockito times assertion depending on a kafka message on tests

--- src/test/java/es/xganie/showcase/kafka/KafkaConsumerTest.java
+++ src/test/java/es/xganie/showcase/kafka/KafkaConsumerTest.java
@@ -1,31 +1,44 @@
 package es.xganie.showcase.kafka;
 
+import es.xganie.showcase.service.RecordProcessorService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 @DirtiesContext
-@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 class KafkaConsumerTest {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @MockBean
+    RecordProcessorService recordProcessorService;
 
     @Value("${showcase.kafka.topic}")
     private String topic;
 
+    @DisplayName("When a message is sent to kafka topic, then the message is process with a service")
     @Test
-    void sendSomeMessageAndPrintWithoutAssert() throws InterruptedException {
+    void whenMessageIsSentToKafka_thenProcessIsCalledWithMessage() {
         kafkaTemplate.send(topic, "Hello World");
-        Thread.sleep(1000); // Add some sleep to see the message "Hello world" on the log
+        await()
+                .atMost(2, TimeUnit.SECONDS)
+                .untilAsserted(() -> verify(recordProcessorService, times(1)).process(anyString()));
     }
 
 }
